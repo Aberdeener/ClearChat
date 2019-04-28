d06e71c210a24cbbbcd46ce2aeffb0fd728b9451
Tadhg <tadhgsmboyle@gmail.com>
2019-04-27 23:06:17 -0700

    Version 2.0
    Added ability to edit messages via config.yml

--- src/me/aberdeener/clearchat/ClearChat.java
+++ src/me/aberdeener/clearchat/ClearChat.java
@@ -8,18 +8,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClearChat extends JavaPlugin{
-	
+
+    public void onEnable() {
+    	getConfig().options().copyDefaults(true);
+    	saveConfig();
+    }
+    
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
         
         if(commandLabel.equalsIgnoreCase("clearchat") || commandLabel.equalsIgnoreCase("cc")) {
         	Player p = (Player)sender;
         	if(!p.hasPermission("clear.chat")) {
-        		p.sendMessage(ChatColor.RED + "Oops ya got no perms D:");
+        		p.sendMessage(getConfig().getString("no-permission"));
         	}else {
         		for(int i=0;i<200;i++) {
         			Bukkit.broadcastMessage("");
         		}
-        		p.sendMessage(ChatColor.GREEN + "Cleared chat for ya :D");
+        		p.sendMessage(getConfig().getString("chat-cleared"));
         	}
         }	
         	
